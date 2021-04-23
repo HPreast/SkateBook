0ff9eb8b621b4d256b8a8e619fe774bad29cc711
HPreast <preasths1@gmail.com>
2021-04-23 14:29:13 -0500

    Implemented logic to display list of trick cards showing the name of the trick and a button to learn more about the trick on Trick List page

--- src/components/tricks/TrickList.js
+++ src/components/tricks/TrickList.js
@@ -1,2 +1,37 @@
 //Display List of Tricks for users to select from
-import React from "react"
+import React, { useEffect, useState } from "react"
+import { useHistory } from "react-router";
+import { getTricks } from "../../modules/TrickManager";
+import { TrickCard } from "./TrickCard";
+// import { trickList } from "../../images/trickList.jpg"
+
+export const TrickList = () => {
+    const [tricks, setTricks] = useState([]);
+
+    const displayTricks = () => {
+        return getTricks()
+        .then(items => {
+            let trickDisplay = items
+            setTricks(trickDisplay)
+        })
+    }
+    useEffect(() => {
+        displayTricks()
+    }, [])
+    console.log(tricks)
+    
+    return (
+        <section className="trickList">
+            <h3 className="listHeader">Trick List</h3>
+            {/* img link is currently broken */}
+            <img src={require("../../images/trickList.jpg")} alt="Person doing a trick on a skateboard" />
+            <div className="listContainer">
+                {tricks.map(trick => 
+                    <TrickCard 
+                    key={trick.id}
+                    trick={trick} />
+                )}
+            </div>
+        </section>
+    )
+}

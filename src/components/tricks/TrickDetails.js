dda9d2f2d77ecf97835b13f46359c5236de4fe8b
HPreast <preasths1@gmail.com>
2021-05-03 16:10:22 -0500

    Attempting to disable buttons to add tricks to a users list if that trick has already been added

--- src/components/tricks/TrickDetails.js
+++ src/components/tricks/TrickDetails.js
@@ -2,11 +2,12 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { getTrickById } from "../../modules/TrickManager"
-import { addPractice } from "../../modules/UserManager";
+import { addPractice, getUserPracticeTricks } from "../../modules/UserManager";
 import { YoutubeEmbed } from "./YoutubeEmbed";
 
 export const TrickDetails = () => {
     const [tricks, setTricks] = useState([]);
+    const [relations, setRelations] = useState([])
     const {trickId} = useParams();
 
     const loggedInUser = JSON.parse(sessionStorage.getItem("headspace_user"))
@@ -16,7 +17,14 @@ export const TrickDetails = () => {
         .then(trick =>
             setTricks(trick))
     }
-
+    const trickRelations = () => {
+        getUserPracticeTricks(loggedInUser)
+        .then(response => 
+            setRelations(response)
+            // console.log(response)
+        )
+            
+    }
     const handleAddToPractice = (id) => {
         const newTrick = {
             userId: loggedInUser,
@@ -42,11 +50,32 @@ export const TrickDetails = () => {
         .then(() => displayDetails())
         }
     }
-    
+    useEffect(() => {
+        trickRelations(loggedInUser)
+    }, [trickId])
     useEffect(() => {
         displayDetails(trickId)
     }, [trickId])
     return (
+    <>    
+        {loggedInUser === relations.userId && tricks.id === relations.trickId ?
+        <>
+        <section className="detailContainer">
+            {/* <button type="button" className="btn btn-primary" id="practiceBtn"onClick={() => handleAddToPractice(tricks.id)}>Add to Practice</button>
+            <button type="button" className="btn btn-primary" id="libraryBtn"onClick={() => handleAddToLibrary(tricks.id)}>Add to Library</button> */}
+            <h2 className="detailsName">{tricks.name}</h2><hr></hr>
+            <div className="trickSummary">
+                <p><strong>Description: </strong></p><p className="summary">{tricks.description}</p>
+                <p><strong>Tips: </strong></p><p className="summary">{tricks.tips}</p>
+            </div>
+            <div className="trickGif">
+                <img src={tricks.gif}></img>
+            </div>
+            <div className="embedYT">
+                <YoutubeEmbed embedId={tricks.yt}/>
+            </div>
+        </section>
+        </> : <>
         <section className="detailContainer">
             <button type="button" className="btn btn-primary" id="practiceBtn"onClick={() => handleAddToPractice(tricks.id)}>Add to Practice</button>
             <button type="button" className="btn btn-primary" id="libraryBtn"onClick={() => handleAddToLibrary(tricks.id)}>Add to Library</button>
@@ -62,5 +91,8 @@ export const TrickDetails = () => {
                 <YoutubeEmbed embedId={tricks.yt}/>
             </div>
         </section>
+        </>
+        }
+    </>
     )
 }

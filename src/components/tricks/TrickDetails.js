c603e9406886af642f79dcfa838170e3c5789a58
HPreast <preasths1@gmail.com>
2021-05-04 14:49:51 -0500

    Implemented logic to remove a trick card given the active user has the respective trick included in a personal list

--- src/components/tricks/TrickDetails.js
+++ src/components/tricks/TrickDetails.js
@@ -1,13 +1,17 @@
 //Displays details and tips for a trick when a card is selected from Trick Lists
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
-import { getTrickById } from "../../modules/TrickManager"
+import { getTrickById, getTricks } from "../../modules/TrickManager"
 import { addPractice, getUserPracticeTricks } from "../../modules/UserManager";
 import { YoutubeEmbed } from "./YoutubeEmbed";
 
 export const TrickDetails = () => {
     const [tricks, setTricks] = useState([]);
-    const [relations, setRelations] = useState([])
+    // const [relations, setRelations] = useState([])
+    // const [trickRelations, setTrickRelations] = useState([])
+    // const [allTricks, setAllTricks] = useState([])
+    // const [allTricksNotRelated, setAllTricksNotRelated] = useState([])
+
     const {trickId} = useParams();
 
     const loggedInUser = JSON.parse(sessionStorage.getItem("headspace_user"))
@@ -17,14 +21,39 @@ export const TrickDetails = () => {
         .then(trick =>
             setTricks(trick))
     }
-    const trickRelations = () => {
-        getUserPracticeTricks(loggedInUser)
-        .then(response => 
-            setRelations(response)
-            // console.log(response)
-        )
+
+    // const getAllTricks = () => {
+    //     getTricks()
+    //     .then(response => setAllTricks(response))
+    // }
+
+    // const trickRelationship = () => {
+    //     getUserPracticeTricks(loggedInUser)
+    //     .then(response => {
+    //         console.log(response)
+    //         let relationsObj = response.map(relation => relation.trick)
+    //         setRelations(relationsObj)
+    //         let trickRelationsObj = response.map(userRes => userRes.trickId)
+    //         setTrickRelations(trickRelationsObj)
+    //     })
             
-    }
+    // }
+
+    // const notTricks = () => {
+    //     let notMyTricks = [...allTricks];
+    //     for(var i = 0, len = trickRelations.length; i < len; i++) {
+    //         for(var j = 0, len2 = notMyTricks.length; j < len2; j++) {
+    //             if (trickRelations[i] === notMyTricks[j].id) {
+    //                 notMyTricks.splice(j, 1);
+    //                 len2 = notMyTricks.length
+    //             }
+    //         }
+    //     }
+    //     console.log(notMyTricks)
+    // }
+
+   
+
     const handleAddToPractice = (id) => {
         const newTrick = {
             userId: loggedInUser,
@@ -50,32 +79,29 @@ export const TrickDetails = () => {
         .then(() => displayDetails())
         }
     }
-    useEffect(() => {
-        trickRelations(loggedInUser)
-    }, [trickId])
+
     useEffect(() => {
         displayDetails(trickId)
+    
     }, [trickId])
+
     return (
-    <>    
-        {loggedInUser === relations.userId && tricks.id === relations.trickId ?
-        <>
-        <section className="detailContainer">
-            {/* <button type="button" className="btn btn-primary" id="practiceBtn"onClick={() => handleAddToPractice(tricks.id)}>Add to Practice</button>
-            <button type="button" className="btn btn-primary" id="libraryBtn"onClick={() => handleAddToLibrary(tricks.id)}>Add to Library</button> */}
-            <h2 className="detailsName">{tricks.name}</h2><hr></hr>
-            <div className="trickSummary">
-                <p><strong>Description: </strong></p><p className="summary">{tricks.description}</p>
-                <p><strong>Tips: </strong></p><p className="summary">{tricks.tips}</p>
-            </div>
-            <div className="trickGif">
-                <img src={tricks.gif}></img>
-            </div>
-            <div className="embedYT">
-                <YoutubeEmbed embedId={tricks.yt}/>
-            </div>
-        </section>
-        </> : <>
+        // <section className="detailContainer">
+        //     {/* <button type="button" className="btn btn-primary" id="practiceBtn"onClick={() => handleAddToPractice(tricks.id)}>Add to Practice</button>
+        //     <button type="button" className="btn btn-primary" id="libraryBtn"onClick={() => handleAddToLibrary(tricks.id)}>Add to Library</button> */}
+        //     <h2 className="detailsName">{tricks.name}</h2><hr></hr>
+        //     <div className="trickSummary">
+        //         <p><strong>Description: </strong></p><p className="summary">{tricks.description}</p>
+        //         <p><strong>Tips: </strong></p><p className="summary">{tricks.tips}</p>
+        //     </div>
+        //     <div className="trickGif">
+        //         <img src={tricks.gif}></img>
+        //     </div>
+        //     <div className="embedYT">
+        //         <YoutubeEmbed embedId={tricks.yt}/>
+        //     </div>
+        // </section>
+        
         <section className="detailContainer">
             <button type="button" className="btn btn-primary" id="practiceBtn"onClick={() => handleAddToPractice(tricks.id)}>Add to Practice</button>
             <button type="button" className="btn btn-primary" id="libraryBtn"onClick={() => handleAddToLibrary(tricks.id)}>Add to Library</button>
@@ -91,8 +117,6 @@ export const TrickDetails = () => {
                 <YoutubeEmbed embedId={tricks.yt}/>
             </div>
         </section>
-        </>
-        }
-    </>
+   
     )
 }
--- src/components/tricks/TrickList.js
+++ src/components/tricks/TrickList.js
@@ -5,20 +5,73 @@ import { getTricks } from "../../modules/TrickManager";
 import { TrickCard } from "./TrickCard";
 import trickList from "../images/trickList.jpg"
 import "./Tricks.css"
+import { getUserPracticeTricks } from "../../modules/UserManager"
 
 export const TrickList = () => {
     const [tricks, setTricks] = useState([]);
+    // const [relations, setRelations] = useState([])
+    const [trickRelations, setTrickRelations] = useState([])
+    const [allTricks, setAllTricks] = useState([])
+    const [allTricksNotRelated, setAllTricksNotRelated] = useState([])
 
-    const displayTricks = () => {
-        return getTricks()
-        .then(items => {
-            let trickDisplay = items
-            setTricks(trickDisplay)
-        })
+    const loggedInUser = JSON.parse(sessionStorage.getItem("headspace_user"))
+
+    // const displayTricks = () => {
+    //     return getTricks()
+    //     .then(items => {
+    //         let trickDisplay = items
+    //         setTricks(allTricksNotRelated)
+    //     })
+    // }
+
+    const getAllTricks = () => {
+        getTricks()
+        .then(response => setAllTricks(response))
     }
+
+    const trickRelationship = () => {
+        getUserPracticeTricks(loggedInUser)
+        .then(response => {
+            // console.log(response)
+            // let relationsObj = response.map(relation => relation.trick)
+            // setRelations(relationsObj)
+            let trickRelationsObj = response.map(userRes => userRes.trickId)
+            setTrickRelations(trickRelationsObj)
+        })
+            
+    }
+
+    const notTricks = () => {
+           
+            let notMyTricks = [...allTricks];
+            console.log(notMyTricks)
+            for(var i = 0, len = trickRelations.length; i < len; i++) {
+                for(var j = 0, len2 = notMyTricks.length; j < len2; j++) {
+                    if (trickRelations[i] === notMyTricks[j].id) {
+                        notMyTricks.splice(j, 1);
+                        len2 = notMyTricks.length
+                    }
+                }
+            }
+            // console.log(notMyTricks)
+            setTricks(notMyTricks)
+    }
+    // console.log(allTricksNotRelated)
+    // console.log(tricks)
+    // console.log(allTricks)
+    // console.log(trickRelations)
+    // console.log(relations)
     useEffect(() => {
-        displayTricks()
+        // displayTricks()
+        // console.log("allTricks")
+        getAllTricks()
+        trickRelationship()
+        
     }, [])
+    useEffect(() => {
+        // console.log("trickList")
+        notTricks()
+    }, [trickRelations])
     
     
     return (

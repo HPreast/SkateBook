4bbfe17927f6a00df0f84298ca7da47908f86b76
HPreast <preasths1@gmail.com>
2021-05-06 21:11:36 -0500

    Fixed bugged filter that was preventing trick list to populate

--- src/components/tricks/TrickList.js
+++ src/components/tricks/TrickList.js
@@ -8,71 +8,48 @@ import "./Tricks.css"
 import { getUserPracticeTricks } from "../../modules/UserManager"
 
 export const TrickList = () => {
-    const [tricks, setTricks] = useState([]); 
+    const [tricks, setTricks] = useState([]);
     const [trickRelations, setTrickRelations] = useState([])
     const [allTricks, setAllTricks] = useState([])
-
     const loggedInUser = JSON.parse(sessionStorage.getItem("headspace_user"))
-
-    
+    console.log(tricks)
     const getAllTricks = () => {
-        getTricks()
-        .then(response => setAllTricks(response))
-        // setTricks(allTricks)
+        return getTricks().then(allTricksFromAPI => setAllTricks(allTricksFromAPI))
     }
-
     const trickRelationship = () => {
-        getUserPracticeTricks(loggedInUser)
-        .then(response => {
-            let trickRelationsObj = response.map(userRes => userRes.trickId)
-            setTrickRelations(trickRelationsObj)
-            console.log("trickRelations", trickRelations)
-        })
+        return getUserPracticeTricks(loggedInUser).then(userPracticeTricksFromAPI => {
+                let trickRelationsObj = userPracticeTricksFromAPI.map(userRes => userRes.trickId)
+                setTrickRelations(trickRelationsObj)
+            })
     }
-
     const notTricks = () => {
-        const resultArray = await Promise.all([getAllTricks(), trickRelationship()])
-        .then(() => {
-            
         let notMyTricks = [...allTricks];
-        for(var i = 0, len = trickRelations.length; i < len; i++) {
-            for(var j = 0, len2 = notMyTricks.length; j < len2; j++) {
+        for (var i = 0, len = trickRelations.length; i < len; i++) {
+            for (var j = 0, len2 = notMyTricks.length; j < len2; j++) {
                 if (trickRelations[i] === notMyTricks[j].id) {
                     notMyTricks.splice(j, 1);
                     len2 = notMyTricks.length
                 }
             }
         }
-        console.log("notMyTricks", notMyTricks)
         setTricks(notMyTricks)
-        console.log("tricks",tricks)
-        console.log("resultArray", resultArray)
-    });
     }
-    
     useEffect(() => {
         getAllTricks()
-        .then(() => trickRelationship())
-        .then(() => notTricks())
-        
-        
-        
+            .then(() => trickRelationship())
     }, [])
-    // useEffect(() => {
-    //     notTricks()
-        
-    // }, [])
-    
-    
+    useEffect(() => {
+        notTricks()
+    }, [trickRelations])
     return (
         <section className="trickList">
             <h2 className="listHeader">Trick List</h2><hr></hr>
             <img id="trickListImg" src={trickList} alt="Person doing a trick on a skateboard" />
             <div className="listContainer">
-                {tricks.map(trick => 
-                    <TrickCard 
-                    key={trick.id}
-                    trick={trick} />
+                {tricks.map(trick =>
+                    <TrickCard
+                        key={trick.id}
+                        trick={trick} />
                 )}
             </div>
         </section>
